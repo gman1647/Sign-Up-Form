a2eca3b5f0304988975e6e36fa7be5090d97d8bb
Greg Baughman <75804705+gman1647@users.noreply.github.com>
2023-04-29 23:20:32 -0400

    Add Event Listener to Add/Remove Required Border
    Add function and event listener to add or remove a border around
    required elements if text field is empty. Works as intended, but may try
    to update to forEach function.

--- myscripts.js
+++ myscripts.js
@@ -8,38 +8,21 @@ Alert successful form submission (with disclaimer that it was not submitted)
 Remove "error" class when text entered
 */
 
+let errorBorderElements = document.getElementsByClassName('required');
 
-
-//let emptyField = document.getElementsByClassName('error')[0].value;
-
-function focusOut() {
-    let removedHighligth = document.getElementById('first_name');
-    if (removedHighligth.value != '') {
-        console.log(removedHighligth.value);
-        removedHighligth.classList.remove('error');
-    } else if(removedHighligth.value == '') {
-        console.log("no text");
-        removedHighligth.classList.add('error');
-    }
-} 
-
-let hilightedElements = document.getElementById('first_name');
-hilightedElements.addEventListener('focusout', focusOut);
-
-/*
-function focusOut() {
-    let removedHighligth = document.getElementsByClassName('required');
-    if (removedHighligth.value != '') {
-        console.log(removedHighligth.value);
-        removedHighligth.classList.remove('error');
-    } else if(removedHighligth.value == '') {
-        console.log("no text");
-        removedHighligth.classList.add('error');
-    }
-} 
-
-let hilightedElements = document.getElementsByClassName('required');
-for(i = 0; i < hilightedElements.length; i++) {
-    hilightedElements[i].addEventListener('focusout', focusOut);
+for(let i=0; i<errorBorderElements.length; i++)
+{
+    errorBorderElements[i].addEventListener('focusout', function(){
+    focusOut(errorBorderElements[i]);
+    }, false);
 }
-*/
+
+function focusOut(removedHighlight) {
+    if (removedHighlight.value != '') {
+        console.log(removedHighlight.value);
+        removedHighlight.classList.remove('error');
+    } else if(removedHighlight.value == '') {
+        console.log("no text");
+        removedHighlight.classList.add('error');
+    }
+} 
